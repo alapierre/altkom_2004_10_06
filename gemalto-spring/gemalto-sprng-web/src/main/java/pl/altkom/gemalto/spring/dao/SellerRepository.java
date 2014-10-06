60fb5c8a5a46cdf27b3a2c0c8f6768cd0c5f949e
mmierzyn <mmierzyn@gemalto.com>
2014-10-06 12:33:36 +0200

    SellerRepo - Marek

---
+++ gemalto-spring/gemalto-sprng-web/src/main/java/pl/altkom/gemalto/spring/dao/SellerRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.altkom.gemalto.spring.dao;
+
+import java.io.Serializable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.altkom.gemalto.spring.model.Seller;
+
+/**
+ *
+ * @author mmierzyn
+ */
+public interface SellerRepository extends JpaRepository<Long, Seller>{
+    
+}
