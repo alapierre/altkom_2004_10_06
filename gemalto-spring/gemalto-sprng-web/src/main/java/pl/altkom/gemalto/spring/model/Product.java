634b7f924e6aaf550f1f21fc80d6c35a105107f7
Arek Drzewiecki <arkadiusz.drzewiecki@gemalto.com>
2014-10-06 11:16:25 +0200

    Added product.

---
+++ gemalto-spring/gemalto-sprng-web/src/main/java/pl/altkom/gemalto/spring/model/Product.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.altkom.gemalto.spring.model;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author adrzewiecki
+ */
+@Entity
+public class Product extends BaseEntity{
+
+    private String name;
+
+    private Double price;
+
+    private String code;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
