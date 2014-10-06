5a1b64c074c5722b6ae153821178b7cf3a6e2978
mchodun <mchodun@DCWN2R1.gemalto.com>
2014-10-06 12:53:04 +0200

    InvoiceItem repository creation

---
+++ gemalto-spring/gemalto-sprng-web/src/main/java/pl/altkom/gemalto/spring/services/InvoiceItemService.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.altkom.gemalto.spring.services;
+
+import pl.altkom.gemalto.spring.dao.InvoiceItemRepository;
+import pl.altkom.gemalto.spring.model.InvoiceItem;
+
+/**
+ *
+ * @author mchodun
+ */
+public interface InvoiceItemService {
+
+    void addItem(InvoiceItem invoiceItem);
+
+    InvoiceItemRepository getInvoiceItemRepository();
+
+    void setInvoiceItemRepository(InvoiceItemRepository invoiceItemRepository);
+
+    void deleteItem(InvoiceItem invoiceItem);
+    
+}
