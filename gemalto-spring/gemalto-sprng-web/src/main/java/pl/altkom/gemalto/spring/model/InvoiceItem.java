9f9dfd50d7489af329334d61fcab3090cb2c52f8
mchodun <mchodun@DCWN2R1.gemalto.com>
2014-10-06 11:17:11 +0200

    creation

---
+++ gemalto-spring/gemalto-sprng-web/src/main/java/pl/altkom/gemalto/spring/model/InvoiceItem.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.altkom.gemalto.spring.model;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author mchodun
+ */
+@Entity
+public class InvoiceItem extends BaseEntity {
+    private long InvoiceId;
+    private float priceNet;
+    private float priceGross;
+    private String name;
+    private String description;
+
+    public long getInvoiceId() {
+        return InvoiceId;
+    }
+
+    public void setInvoiceId(long InvoiceId) {
+        this.InvoiceId = InvoiceId;
+    }
+
+    public float getPriceNet() {
+        return priceNet;
+    }
+
+    public void setPriceNet(float priceNet) {
+        this.priceNet = priceNet;
+    }
+
+    public float getPriceGross() {
+        return priceGross;
+    }
+
+    public void setPriceGross(float priceGross) {
+        this.priceGross = priceGross;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
